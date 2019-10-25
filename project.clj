5cc7c528e0bb0c57133e12252173aa0ed8fe1185
Burin Choomnuan <agilecreativity@gmail.com>
2019-10-25 11:03:53 +1100

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject net.b12n/swiza-sh "0.1.1"
+(defproject net.b12n/swiza-sh "0.1.2"
   :description "swiza-sh interact with shell from Clojure"
   :url "http://github.com/agilecreativity/swiza-sh"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljfmt "0.6.1"]
-            [jonase/eastwood "0.3.5"]
+  :plugins [[lein-cljfmt "0.6.4"]
+            [jonase/eastwood "0.3.6"]
             [lein-auto "0.1.3"]
-            [lein-cloverage "1.0.13"]]
+            [lein-cloverage "1.1.2"]]
   :dependencies [[net.b12n/swiza-commons "0.1.2"]
                  [org.clojars.hozumi/clj-commons-exec "1.2.0"]
                  [org.clojure/clojure "1.9.0"]]
@@ -16,6 +16,6 @@
                "src/test/java"]
   :target-path "target/%s"
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[org.clojure/test.check "0.9.0"]
+                   :dependencies [[org.clojure/test.check "0.10.0"]
                                   [jonase/eastwood "0.3.6"]]}
              :uberjar {:aot :all}})
